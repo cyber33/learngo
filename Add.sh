f80a30ca2d095aa512ba2a6dee2f4d4b368d95ad
Mark Yoshikawa <mark.yoshikawa@gmail.com>
2022-01-17 23:59:49 +0000

    Try it for your self and add notes of learning and futher research done

---
+++ Add.sh
@@ -0,0 +1,12 @@
+git add -A ./06-variables/01-basic-data-types/exercises/01-print-the-literals/solution/TryIt.txt
+git add -A ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/Arabic.txt
+git add -A ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/englishAndArabic.go
+git add -A ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/main.go
+git add -A ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/UTF-8.pdf
+git add -A ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/writeArabicToFile.go
+git add -A ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt.txt
+git add -A ./06-variables/01-basic-data-types/exercises/02-print-hexes/TryIt/main.go
+git add -A ./06-variables/01-basic-data-types/exercises/02-print-hexes/TryIt.txt
+git add -A ./06-variables/01-basic-data-types/questions/TryIt.txt
+git add -A ./06-variables/02-declarations/01-declaration-syntax/01-syntax/TryIt.txt
+
