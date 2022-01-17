af9d528be36da5ce6ea17f159caf764c2cf5c4b9
Mark Yoshikawa <mark.yoshikawa@gmail.com>
2022-01-17 23:59:57 +0000

    Try it for your self and add notes of learning and futher research done

---
+++ Commit.sh
@@ -0,0 +1,11 @@
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/01-print-the-literals/solution/TryIt.txt
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/Arabic.txt
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/englishAndArabic.go
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/main.go
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/UTF-8.pdf
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt/writeArabicToFile.go
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/01-print-the-literals/TryIt.txt
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/02-print-hexes/TryIt/main.go
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/exercises/02-print-hexes/TryIt.txt
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/01-basic-data-types/questions/TryIt.txt
+git commit -m "Try it for your self and add notes of learning and futher research done" ./06-variables/02-declarations/01-declaration-syntax/01-syntax/TryIt.txt
